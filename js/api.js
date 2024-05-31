60591316cb919d6a9ca23b8586430b70f375ceac
Valeria <vaalcarrera@gmail.com>
2024-05-31 19:08:13 +0200

    avance con detalle.html; avance con el consumo de api.

--- js/api.js
+++ js/api.js
@@ -1,16 +1,9 @@
-const url = 'https://movie-database-alternative.p.rapidapi.com/?r=json&i=tt4154796';
+const API_SERVER = 'https://api.themoviedb.org/3';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '2c148bde5bmshffb7a86f65ad9d6p1e3338jsn415eb73e51b0',
-		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTJjYTAwZDYxZWIzOTEyYjZlNzc4MDA4YWQ3ZmNjOCIsInN1YiI6IjYyODJmNmYwMTQ5NTY1MDA2NmI1NjlhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4MJSPDJhhpbHHJyNYBtH_uCZh4o0e3xGhZpcBIDy-Y8'
+        
+    }
 };
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
