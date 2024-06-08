837c7b1bf3792b4fa5c01c0e888dac7bb3280a12
Valeria <vaalcarrera@gmail.com>
2024-06-08 03:21:23 +0200

    Consumo de api aplicado

--- js/api.js
+++ js/api.js
@@ -1,9 +1,48 @@
-const API_SERVER = 'https://api.themoviedb.org/3';
-const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTJjYTAwZDYxZWIzOTEyYjZlNzc4MDA4YWQ3ZmNjOCIsInN1YiI6IjYyODJmNmYwMTQ5NTY1MDA2NmI1NjlhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4MJSPDJhhpbHHJyNYBtH_uCZh4o0e3xGhZpcBIDy-Y8'
-        
-    }
-};
+(() => {
+    console.log('Script cargado'); // Para verificar que el script se ha cargado
+
+    const cargarPeliculas = async () => {
+        try {
+            console.log('Iniciando la carga de películas'); // Para verificar que la función se llama
+
+            const respuesta = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=86c391894dbd0937ad4c471416a6ca2a&language=es-MX');
+
+            console.log('Respuesta recibida', respuesta); // Para verificar la respuesta de la API
+
+            if (respuesta.status === 200) {
+                const datos = await respuesta.json();
+                console.log('Datos recibidos', datos); // Para verificar los datos recibidos
+
+                let peliculas = '';
+
+                datos.results.forEach(pelicula => {
+                    if (pelicula.poster_path) {
+                        peliculas += `
+                            <div>
+                                <img class="poster hvr-pulse-grow" src="https://image.tmdb.org/t/p/w200/${pelicula.poster_path}" alt="${pelicula.title}">
+                                <h3>${pelicula.title}</h3>
+                            </div>
+                        `;
+                    } else {
+                        console.log(`La película ${pelicula.title} no tiene poster_path`);
+                    }
+                });
+
+                document.getElementById('contenedor__populares').innerHTML = peliculas;
+                console.log('Películas cargadas en el DOM'); // Para verificar que se ha actualizado el DOM
+
+            } else if (respuesta.status === 401) {
+                console.log('Clave de API incorrecta');
+            } else if (respuesta.status === 404) {
+                console.log('No se encontraron películas');
+            } else {
+                console.log('Hubo un error desconocido');
+            }
+
+        } catch (error) {
+            console.log('Error de conexión o algo salió mal: ', error);
+        }
+    };
+
+    cargarPeliculas();
+})();
